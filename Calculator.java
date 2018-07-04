77acdeeeb850fd3507d56642ec8562d80acda4df
TELUS\T931906 <Melvin.Mathews@telus.com>
2018-07-04 15:30:54 -0700

    completed

--- Calculator.java
+++ Calculator.java
@@ -10,35 +10,37 @@ public class Calculator {
 
     public static Stack<String> numbers = new Stack<>();
     public static Stack<String> operations = new Stack<>();
+       
     
     public static void main(String[] args) {
-        double result = Double.parseDouble(args[0]);
-        double output; 
-        String operation = "a";
-
-
+        double output = Double.parseDouble(args[0]);
+                
         for (int i = 0; i < args.length; i++) {
             math(args[i]);
+            output = calculate(args[i], i, args, output); 
         }
-        System.out.printf("%f%n", result);
 
+        System.out.print(numbers.toString() + "\n");
+        System.out.print(operations.toString() + "\n");
+        System.out.print(output + "\n");
+                
     }
 
     public static void math(String input) {
 
-        if (input == "+") {
+        if (input.equals("+")) {
             operations.push(input);
         }
 
-        else if (input == "-") {
+        else if (input.equals("-")) {
             operations.push(input);
         }
 
-        else if (input == "x") {
+        else if (input.equals("x")) {
             operations.push(input);
         }
 
-        else if (input == "/") {
+        else if (input.equals("/")) {
             operations.push(input);
         }
 
@@ -46,6 +48,34 @@ public class Calculator {
             numbers.push(input);
         }
 
+    }
+
+    public static double calculate(String input, int i, String[] args, double result){
+        double value;
+        
+        
+        if (input.equals("+")) {
+            value =  Double.parseDouble(args[i+1]);
+            result += value;
+        }
+
+        else if (input.equals("-")) {
+            value =  Double.parseDouble(args[i+1]);
+            result -= value;
+        }
+
+        else if (input.equals("x")) {
+            value =  Double.parseDouble(args[i+1]);
+            result *= value;
+        }
+
+        else if (input.equals("/")) {
+            value =  Double.parseDouble(args[i+1]);
+            result /= value;
+        }
+
+        return result;
+        
     }
 
 
